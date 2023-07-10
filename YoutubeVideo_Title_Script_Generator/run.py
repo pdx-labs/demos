afbd3583946a4173a960cc09d18bdcd12ed70beb
vxw8 <126307514+vxw8@users.noreply.github.com>
2023-07-10 09:38:50 -0700

    changes made

--- YoutubeVideo_Title_Script_Generator/run.py
+++ YoutubeVideo_Title_Script_Generator/run.py
@@ -4,7 +4,7 @@ from pdx.settings import Keys
 from title_agent import title_agent
 from script_agent import script_agent
 
-# Set up the api key using your own API key using the command "set API_KEY=your_api_key"
+# Set up the api key using your own API key using the command "set OPENAI_KEY=your_api_key"
 # for WINDOWS operating system in command prompt terminal and then retrieve key
 openai_key = os.environ.get('OPENAI_KEY')
 api_keys = Keys(openai_key=openai_key)
@@ -22,7 +22,7 @@ if __name__ == '__main__':
 
     _script_response = script_agent.execute({
         "script_template": {
-            "script": _title_response
+            "title": _title_response
         }
     })
 
