caceb201aecd5dece5c6974df4f2f75de287caa8
Adithya Krishnan <krishsandeep@gmail.com>
2023-07-27 14:43:21 +0200

    Update: for version 0.5.0 refactor

--- Best_Computer_Generator/computer_specifications_generator/__init__.py
+++ Best_Computer_Generator/computer_specifications_generator/__init__.py
@@ -1,9 +1,5 @@
 import os
 from pdx import Agent 
-from pdx.settings import Keys
-
-openai_key = os.environ.get('OPENAI_KEY')
-api_keys = Keys(openai_key=openai_key)
 
 agent_folder_path = os.path.dirname(__file__)
-computer_specifications_agent = Agent(agent_folder_path, api_keys=api_keys)
+computer_specifications_agent = Agent(agent_folder_path)
--- Best_Computer_Generator/job_description_generator/__init__.py
+++ Best_Computer_Generator/job_description_generator/__init__.py
@@ -1,9 +1,5 @@
 import os
-from pdx import Agent 
-from pdx.settings import Keys
-
-openai_key = os.environ.get('OPENAI_KEY')
-api_keys = Keys(openai_key=openai_key)
+from pdx import Agent
 
 agent_folder_path = os.path.dirname(__file__)
-job_description_agent = Agent(agent_folder_path, api_keys=api_keys)
+job_description_agent = Agent(agent_folder_path)
