9f8fbbfde5064396f009ef0569c08bab2855e0fb
Adithya Krishnan <krishsandeep@gmail.com>
2023-07-27 14:48:35 +0200

    Update: for version 0.5.0 refactor

--- YoutubeVideo_Title_Script_Generator/run.py
+++ YoutubeVideo_Title_Script_Generator/run.py
@@ -1,15 +1,8 @@
-import os
-from pdx import Agent
-from pdx.settings import Keys
 from title_agent import title_agent
 from script_agent import script_agent
 
-# Set up the api key using your own API key using the command "set OPENAI_KEY=your_api_key"
-# for WINDOWS operating system in command prompt terminal and then retrieve key
-openai_key = os.environ.get('OPENAI_KEY')
-api_keys = Keys(openai_key=openai_key)
 
-#User input: user can enter in any topic they want to generate a youtube title and script about that topic
+# User input: user can enter in any topic they want to generate a youtube title and script about that topic
 
 # Processes user input and provides output
 if __name__ == '__main__':
@@ -22,10 +15,10 @@ if __name__ == '__main__':
 
     _script_response = script_agent.execute({
         "script_template": {
-            "title": _title_response
+            "title": _title_response.data
         }
     })
 
-# Print title and the script
-print(f"Topic: {_topic}")
-print(_script_response.completion)
+    # Print title and the script
+    print(f"Topic: {_topic}")
+    print(_script_response.data)
